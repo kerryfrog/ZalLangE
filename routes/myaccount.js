db2a8f5af7a7fff7dfaa3ff523aea52073ff84bc
synodical <lena0323park@naver.com>
2022-01-25 00:29:31 +0900

    image_src 튜플 1행 고치기

--- routes/myaccount.js
+++ routes/myaccount.js
@@ -9,17 +9,21 @@ var router = express.Router();
 router.get('/', ensureLoggedIn(),
   function (req, res, next) {
     // 해당 계정이 가지고 있는 img src 불러오기 
-    db.get("SELECT image_src FROM images WHERE owner=?",
+    db.get("SELECT image_src, count(*) FROM images WHERE owner=?",
       [req.user.id],
-      function (err, row) {
+      function (err, rows) {
         if (err) { return next(err); }
-        console.log("query answer is :", row);
-        res.render('profile.html', {
-          user: req.user,
-          src_row: row
-        });
+        if (rows != undefined) {
+          console.log(rows);
+          (rows).forEach((row) => {
+            console.log("**:", row);
+            res.render('profile.html', {
+              user: req.user,
+              src_row: rows
+            });
+          });
+        }
       } //end function(err , row )
-
     ) // end db.get     
   }); //end router.get
 
