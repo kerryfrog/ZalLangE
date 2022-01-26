e3255a353ee5be20e6d6850921a024758757a757
synodical <lena0323park@naver.com>
2022-01-26 23:41:38 +0900

    Bug Fix: Get all row of img_src from DB

--- routes/myaccount.js
+++ routes/myaccount.js
@@ -9,26 +9,17 @@ var router = express.Router();
 router.get('/', ensureLoggedIn(),
   function (req, res, next) {
     // 해당 계정이 가지고 있는 img src 불러오기 
-    db.get("SELECT image_src, count(*) FROM images WHERE owner=?",
+    db.all("SELECT image_src FROM images WHERE owner=?", //all method: 디비에서 모든 결과를 불러옴
       [req.user.id],
       function (err, rows) {
         if (err) { return next(err); }
-        console.log(rows);
-        res.render('profile.html', {
-          user: req.user,
-          src_row: rows
-        });/*
         if (rows != undefined) {
           console.log(rows);
-          (rows).forEach((row) => {
-            console.log("**:", row);
-            res.render('profile.html', {
-              user: req.user,
-              src_row: rows
-            });
+          res.render('profile.html', {
+            user: req.user,
+            src_row: rows
           });
         }
-        */
       } //end function(err , row )
     ) // end db.get     
   }); //end router.get
